f6b15f3f1a99aa64b3c2dd691e0461065c8bd828
JONAH Joseph <132363030+Joses47@users.noreply.github.com>
2024-04-09 11:11:16 -0700

    incomplete testimonial section. worked on the js and css already

--- Scripts/script.js
+++ Scripts/script.js
@@ -49,3 +49,58 @@ const experienceInterval = setInterval(() => {
         clearInterval(experienceInterval)
     }
 }, 600);
+
+
+
+
+var testimonials = [
+  {
+      name: "Jack Thompson",
+      image: "/Images/Testimonials/1712481294302.jpg",
+      message: "I wasn't sure if online learning was for me, but Smartducation proved me wrong! The program's user-friendly interface and dynamic courses have made my learning experience enjoyable and rewarding."
+  },
+  {
+      name: "Emily P Johnson",
+      image: "/Images/Testimonials/fifth.jpg",
+      message: "The interactive lessons and engaging content make learning fun and accessible. Smartducation has equipped me with the skills and knowledge to excel academically and prepare for my future. I'm grateful for this invaluable opportunity!"
+  },
+  {
+      name: "Ethan Rodrygo",
+      image: "/Images/Testimonials/happy-teenage-boy.jpg",
+      message: "The comprehensive curriculum and engaging resources have enriched my knowledge and expanded my skill set in ways I never thought possible."
+  },
+  {
+      name: "Sophia Carter",
+      image: "/Images/Testimonials/fourthFemale.jpg",
+      message: "As a working professional with a hectic schedule, I was skeptical about online education. The flexibility to learn at my own pace and the clarity of instruction from your dedicated tutors made all the difference"
+  }
+];
+
+
+// Index to keep track of the current testimonial
+var currentIndex = 0;
+
+// Function to change testimonial to the next one
+function changeTestimonial() {
+    var testimonial = testimonials[currentIndex];
+    
+    // Update testimonial content
+    var testimonialContainer = document.getElementById('testimonialContainer');
+    testimonialContainer.innerHTML = `
+        <div class="testimonial">
+            <img src="${testimonial.image}">
+            <h4>${testimonial.name}</h4>
+            <p>${testimonial.message}</p>
+        </div>
+    `;
+    
+    //Increment index to get the next testimonial
+    currentIndex = (currentIndex + 1) % testimonials.length;
+}
+
+//Call the function initially to display the first testimonial
+changeTestimonial();
+
+//Set interval to change testimonials every 5 seconds
+setInterval(changeTestimonial, 5000);
+
