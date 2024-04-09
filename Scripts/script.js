5364d08d96f3a00b1a7f68f15eee1a312d1ca8e6
JONAH Joseph <132363030+Joses47@users.noreply.github.com>
2024-04-09 07:37:59 -0700

    comment

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,4 @@
+//Javascript code to increment the numbers when user is in view
 const counters = document.querySelectorAll('.counters span');
 const container = document.querySelector('.counters');
 
@@ -36,62 +37,7 @@ window.addEventListener('scroll', () => {
 })
 
 
-// function countdownNumber(elementId, initialValue) {
-//     let value = initialValue;
-//     const element = document.getElementById('students');
-//     const intervalId = setInterval(() => {
-//       value += 8;
-//       element.innerHTML = value;
-//       if (value === 10000) {
-//         clearInterval(intervalId);
-//       }
-//     });
-//   }
-
-
-//   function modulesContdown(modulesId, modulesInitial) {
-//     let value = modulesInitial;
-//     const modules = document.querySelector('#modules');
-
-//     const moduleInterval = setInterval(() => {
-//         value += 4;
-//         modules.innerHTML = value;
-//         if(value === 5000) {
-//             clearInterval(moduleInterval);
-//         }
-//     });
-//   }
-
-//   countdownNumber("students", 0);
-//   modulesContdown("modules", 0)
-
-
-
-// var schoolsAndStudents = document.querySelector('#schools')
-// var yearsOfExperience = document.querySelector('#experience')
-
-
-// const schoolInterval = setInterval(() => {
-//     let schoolInitial = schoolsAndStudents.innerHTML;
-//     schoolInitial++
-//     schoolsAndStudents.innerHTML = schoolInitial
-//     if(schoolInitial === 48) {
-//         clearInterval(schoolInterval)
-//     }
-// }, 130);
-
-// const experienceInterval = setInterval(() => {
-//     let experienceInitial = yearsOfExperience.innerHTML;
-//     experienceInitial++;
-//     yearsOfExperience.innerHTML = experienceInitial
-//     if(experienceInitial === 10) {
-//         clearInterval(experienceInterval)
-//     }
-// }, 600);
-
-
-
-
+// Storing the users data
 var testimonials = [
   {
       name: "Jack Thompson",
