20c65adfce41a3d983cea48277aade6177c3b6ee
JONAH Joseph <132363030+Joses47@users.noreply.github.com>
2024-04-15 21:54:19 -0700

    done with the course section

---
+++ Scripts/Modules/course-data.js
@@ -0,0 +1,69 @@
+export { courses };
+
+// The course data
+const courses = [{
+    id: '123',
+    image: '/Images/Course section-a/Digital marketing.jpg',
+    name: 'Navigating the Digital Landscape: Strategies for Success in a Crowded Market',
+    tutor: 'Dr Emily Blake',
+    rating: {
+        stars: 4.5,
+        count: 2345,
+    },
+    priceInCent: 2099,
+},
+{   
+    id: '2332',
+    image: '/Images/Course section-a/web development.jpeg',
+    name: 'Full-Stack Web Development: Bridging the Gap Between Frontend and Backend',
+    tutor: 'Cameron Reed & Dr. Angela Zi',
+    rating: {
+        stars: 5,
+        count: 1790,
+    },
+    priceInCent: 3089,
+},
+{
+    id: '7368',
+    image: '/Images/Courses/python.jpg',
+    name: 'Automate the Boring Stuffs with Python',
+    tutor: 'Joseph Jonah',
+    rating: {
+        stars: 5,
+        count: 899,
+    },
+    priceInCent: 2899,
+},
+{
+    id: '83636',
+    image: '/Images/Courses/hacking.jpg',
+    name: 'The Ethics of Hacking: A Guide to Being a Responsible Hacker',
+    tutor: 'Kathrine Roy & Chris Morris ',
+    rating: {
+        stars: 4,
+        count: 1945
+    },
+    priceInCent: 4024,
+}, 
+{
+    id: '93736',
+    image: '/Images/Courses/DataAnalytic.jpg',
+    name: 'Getting Started with Data Anlytics from Zero to Hero',
+    tutor: 'Joseph Jonah',
+    rating: {
+        stars: 4.5,
+        count: 658,
+    },
+    priceInCent: 1922,
+},
+{
+    id: '9661p',
+    image: '/Images/Courses/js.jpg',
+    name: 'Javascript for a Successful Career in Web Development',
+    tutor: 'Krill Raymond',
+    rating: {
+        stars: 4,
+        count: 771
+    },
+    priceInCent: 3480,
+}];
--- Scripts/course.js
+++ Scripts/course.js
@@ -1,59 +1,5 @@
-// The course data
-const courses = [{
-    Id: 'uahdt-97uks-jahj-uhj9-91j2',
-    image: '/Images/Course section-a/Digital marketing.jpg',
-    name: 'Navigating the Digital Landscape: Strategies for Success in a Crowded Market',
-    tutor: 'Dr Emily Blake',
-    rating: {
-        stars: 4.5,
-    },
-    priceInCent: 2099,
-},
-{
-    image: '/Images/Course section-a/web development.jpeg',
-    name: 'Full-Stack Web Development: Bridging the Gap Between Frontend and Backend',
-    tutor: 'Cameron Reed & Dr. Angela Zi',
-    rating: {
-        stars: 5
-    },
-    priceInCent: 3089,
-},
-{
-    image: '/Images/Courses/python.jpg',
-    name: 'Automate the Boring Stuffs with Python',
-    tutor: 'Joseph Jonah',
-    rating: {
-        stars: 5
-    },
-    priceInCent: 2899,
-},
-{
-    image: '/Images/Courses/hacking.jpg',
-    name: 'The Ethics of Hacking: A Guide to Being a Responsible Hacker',
-    tutor: 'Kathrine Roy & Chris Morris ',
-    rating: {
-        stars: 4
-    },
-    priceInCent: 4024,
-}, 
-{
-    image: '/Images/Courses/DataAnalytic.jpg',
-    name: 'Getting Started with Data Anlytics from Zero to Hero',
-    tutor: 'Joseph Jonah',
-    rating: {
-        stars: 4.5
-    },
-    priceInCent: 1922,
-},
-{
-    image: '/Images/Courses/js.jpg',
-    name: 'Javascript for a Successful Career in Web Development',
-    tutor: 'Krill Raymond',
-    rating: {
-        stars: 4
-    },
-    priceInCent: 3480,
-}];
+import { courses } from "./Modules/course-data.js";
+import { cart } from "./Modules/cart.js";
 
 //Generating the course html
 let coursesHTML = '';
@@ -61,31 +7,88 @@ let coursesHTML = '';
 courses.forEach(course => {
     coursesHTML +=  
     `
-        <div class="course-card">
-            <img src="${course.image}">
-            <div class="content">
-                <h4>${course.name}</h4>
-                <p>${course.tutor}</p>
-                <img src="/Images/ratings/rating-${course.rating.stars * 10}.png">
-                <p class="price">$${course.priceInCent / 100}</p>
-            
-                <div class="cta">
-                    <button class="cart-btn js-add-to-cart">Add to Cart</button>
-                    <div class="add-to-cart">
-                        <img src="/Images/Icons/checkmark.png"> 
-                    <p>Added</p>
-                    </div>
-                </div>
-            </div>
+    <div class="product-container">
+        <div class="product-image-container">
+        <img class="product-image"
+            src="${course.image}">
         </div>
+    
+        <div class="product-name limit-text-to-2-lines">
+           ${course.name}
+        </div>
+    
+        <div class="product-rating-container">
+        <img class="product-rating-stars"
+            src="/Images/ratings/rating-${course.rating.stars * 10}.png">
+        <div class="product-rating-count">
+            ${course.rating.count}
+        </div>
+        </div>
+
+        <div class="product-price">
+        $${course.priceInCent / 100}
+        </div>
+
+        <div class="product-spacer"></div>
+
+        <div class="added-to-cart 
+        js-added-to-cart-${course.id}">
+        <img src="/Images/Icons/checkmark.png">
+        Added
+        </div>
+
+        <button class="add-to-cart-button
+        js-add-to-cart"
+        data-course-id="${course.id}">
+            Add to Cart
+        </button>
+    </div>
     `
 });
 
-document.querySelector('.js-course-flex').innerHTML = coursesHTML;
+document.querySelector('.js-products-grid').innerHTML = coursesHTML;
 
 
 document.querySelectorAll('.js-add-to-cart').forEach(button => {
     button.addEventListener('click', () => {
-        alert('added')
-    })
-})
+       const courseId = button.dataset.courseId;
+       
+
+       let matchingItem;
+
+        cart.forEach((courseItem) => {
+            if(courseId === courseItem.courseId) {
+                matchingItem = courseItem;
+            }
+        });
+
+        if(matchingItem) {
+            matchingItem.quantity++;
+        } else {
+            cart.push({
+                courseId: courseId,
+                quantity: 1
+            });
+        };
+
+        let cartQuantity = 0;
+
+        cart.forEach((courseItem) => {
+            cartQuantity += courseItem.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+
+
+        //Displays the added message pop-up
+        var addedMsg = document.querySelector
+        (`.js-added-to-cart-${courseId}`);
+        addedMsg.classList.add('show-added-msg');
+   
+        setTimeout(() => {
+            addedMsg.classList.remove('show-added-msg');
+        }, 1200);
+    });
+});
+
