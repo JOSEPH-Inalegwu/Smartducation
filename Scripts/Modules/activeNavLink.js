d47d7ca2c7142dd4dcccf726fb403451cc001971
JONAH Joseph <132363030+Joses47@users.noreply.github.com>
2024-04-19 13:06:09 -0700

    Linked the active nav-link and removed the cart-content url from the contact page

--- Scripts/Modules/activeNavLink.js
+++ Scripts/Modules/activeNavLink.js
@@ -6,6 +6,14 @@ if (pageName === 'index.html') {
     document.querySelector('.home').classList.add('active-link')
 }
 
+if (pageName === 'about.html') {
+  document.querySelector('.about').classList.add('active-link')
+}
+
 if (pageName === 'courses.html') {
   document.querySelector('.courses').classList.add('active-link')
+}
+
+if (pageName === 'contact.html') {
+  document.querySelector('.contact').classList.add('active-link')
 }
